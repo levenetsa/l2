5a621fee6a5de5144d21dec8356c86a859213231
alex <levenecalexey@gmail.com>
2019-03-12 03:17:26 +0300

    Verify CORS works

--- server/src/main/java/com/lev/server/item/ItemController.java
+++ server/src/main/java/com/lev/server/item/ItemController.java
@@ -1,34 +1,13 @@
 package com.lev.server.item;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.sql.SQLException;
-
+@CrossOrigin
 @RestController
 public class ItemController {
 
-    @Autowired
-    ItemService itemService;
-
     @RequestMapping("/items")
-    public String getAllItems() throws SQLException {
-        return itemService.getAllItems();
-    }
-
-    @PutMapping
-    @RequestMapping("/item")
-    public String getAdd(@RequestBody Item item) throws SQLException {
-        return itemService.createNewItem(item);
-    }
-
-    @RequestMapping("/items/{id}")
-    public String getItemById(@PathVariable int id) throws SQLException {
-        return itemService.getItemById(id);
-    }
-
-    @RequestMapping("/items/search")
-    public String search(@RequestParam String text) throws SQLException {
-        return itemService.search(text);
+    public String getAllItems() {
+        return "success";
     }
 }
